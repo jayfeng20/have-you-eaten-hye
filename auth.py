9166cb33668c8101cea6a3c079d716b3e738db46
jayfeng20 <jonathanfeng2001@gmail.com>
2025-02-06 20:28:57 -0500

    initial db setup

--- auth.py
+++
@@ -1,9 +0,0 @@
-from fastapi import Depends, FastAPI, HTTPException, status
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-
-app = FastAPI()
-
-
-@app.get("/test")
-async def test():
-    return {"message": "Hello World"}
